a8b35ed9559375bbd491a5328caf22eca30084a4
alex.laycy <alexlaycy@gmail.com>
2024-05-28 20:45:00 +0100

    feat: initial v1.0 of full-screen button component

---
+++ src/components/Full-Screen-Toggle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Expand } from "grommet-icons";
+
+import { Box, Button, Text } from "grommet";
+
+export const Icon = () => (
+  <Box align="center" pad="large">
+    <Button plain={false} icon={<Expand />} onClick={() => {}} />
+  </Box>
+);
+
+Icon.storyName = "Icon plain";
+
+export default {
+  title: `Controls/Button/Icon plain`,
+};
