5e2d555a037967db350d2f4447ed0efd4d824823
alex.laycy <alexlaycy@gmail.com>
2024-05-30 18:10:11 +0100

    chore: README cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import './App.css'
 import { grommet, Grommet } from "grommet";
 import Simple from "./containers/Footer";
 
-
 function App() {
-
+  
   return (
     <Grommet theme={grommet}>
       <Simple />
