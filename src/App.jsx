63b8f62c260a9b5339319cb27d890ff2390f8630
alex.laycy <alexlaycy@gmail.com>
2024-06-01 13:41:32 +0100

    fix: merge conflicts resolved

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import { FooterContainer }  from "./containers/Footer";
 import { Notepad } from './containers/Notepad';
 import { SaveButton } from './components/SaveButton';
 import { HeaderContainer } from "./containers/Header";
-<<<<<<< HEAD
-import { SidebarContainer } from './containers/Sidebar';
-import { TextResizeToggle}  from './components/TextResizeToggle';
-=======
->>>>>>> 645510544455203fffce61489e89093049f984a1
+import { TextResizeToggle } from './components/TextResizeToggle';
+import { DarkModeToggle } from './components/DarkModeToggle';
 
 function App() {
   
   return (
-<<<<<<< HEAD
     <Grommet theme={grommet}>
       <Page kind="full">
         <HeaderContainer />
@@ -23,14 +19,6 @@ function App() {
         <SaveButton />
         <FooterContainer />
       
-=======
-    <Grommet theme={grommet} cssVars={true}>
-      <HeaderContainer />
-      <Page background="black" kind="full" fill="vertical" flex="grow">
-        <Main align='center' justify='center'>
-          <Notepad />
-        </Main>
->>>>>>> 645510544455203fffce61489e89093049f984a1
       </Page>
       <FooterContainer />
     </Grommet>
