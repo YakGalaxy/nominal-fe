ebca34fc0849af461160fbfc96a65dbcba4a320b
alex.laycy <alexlaycy@gmail.com>
2024-06-06 19:34:42 +0100

    fix: removed fontsize provider from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { grommet, Grommet, Page } from "grommet";
 import { FooterContainer } from "./containers/Footer";
 import { Notepad } from "./containers/Notepad";
 import { HeaderContainer } from "./containers/Header";
-import { FontSizeProvider } from "./components/FontSizeProvider";
+// import { FontSizeProvider } from "./components/FontSizeProvider";
 import "./index.css";
 import "./App.css"; 
 
@@ -17,13 +17,11 @@ function App() {
       themeMode={darkMode ? "dark" : "light"}
       background="background"
     >
-      <FontSizeProvider>
         <Page kind="wide" background="background" height="100vh" flex="grow">
           <HeaderContainer darkMode={darkMode} setDarkMode={setDarkMode} />
           <Notepad />
           <FooterContainer />
         </Page>
-      </FontSizeProvider>
     </Grommet>
   );
 }
