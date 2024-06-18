0b5933cecf9511305e36b84ec69c2c95276a8752
alex.laycy <alexlaycy@gmail.com>
2024-06-18 18:48:01 +0100

    fixed delete button URL

--- src/components/buttons/DeleteButton.jsx
+++ src/components/buttons/DeleteButton.jsx
@@ -2,7 +2,7 @@ import { Box, Button } from "grommet";
 import { Trash } from "grommet-icons";
 import axios from "axios";
 
-const API_URL = "http://localhost:5005";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
 
 export const DeleteButton = ({ docId, onDelete }) => {
   const deleteDocument = async () => {
